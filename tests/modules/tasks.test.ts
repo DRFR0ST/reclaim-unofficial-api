ff47c61190a27075ec764ca02e37110c6bb39f35
Mike Eling <mike.eling97@gmail.com>
2023-12-15 02:28:43 +0100

    :white_check_mark: Rewrite Tasks tests to fix running tests in a sequence

--- tests/modules/tasks.test.ts
+++ tests/modules/tasks.test.ts
@@ -1,11 +1,10 @@
 import { beforeEach, describe, expect, test } from "bun:test";
-import { ReclaimClient, ReclaimTask } from "../../src";
+import { ReclaimClient } from "../../src";
 import { ReclaimTasks } from "../../src/modules/tasks";
 import { ReclaimTaskCreateMock } from "../mocks";
 
 describe("ReclaimTasks", () => {
   let tasks: ReclaimTasks;
-  let testTask: ReclaimTask;
 
   beforeEach(() => {
     const client = new ReclaimClient();
@@ -16,62 +15,40 @@ describe("ReclaimTasks", () => {
     expect(tasks).toBeTruthy();
   });
 
-  describe("create", () => {
-    test("should create a task", async () => {
-      const results = await tasks.create(ReclaimTaskCreateMock);
-      testTask = results;
-
-      expect(results).toBeTruthy();
-      expect(results.id).toBeGreaterThan(0);
-    });
-  });
-
-  describe("search", () => {
-    test("should list all tasks", async () => {
-      const results = await tasks.search();
-
-      expect(results).toBeTruthy();
-      expect(results.length).toBeGreaterThan(0);
-    });
-
-    test("should search tasks by title", async () => {
-      const results = await tasks.search({
-        title: ReclaimTaskCreateMock.title,
-      });
-
-      expect(results).toBeTruthy();
-      expect(results.length).toBeGreaterThan(0);
-      expect(results[0].id).toBeGreaterThan(0);
-      expect(results[0].title).toBe(ReclaimTaskCreateMock.title);
-    });
-  });
-
-  describe("update", () => {
-    test("should update a task", async () => {
-      const results = await tasks.update(testTask.id, {
-        title: "Updated Test Task 1",
-      });
-
-      expect(results).toBeTruthy();
-      expect(results.id).toBe(testTask.id);
-      expect(results.title).toBe("Updated Test Task 1");
-    });
-  });
-
-  describe("get", () => {
-    test("should get a task", async () => {
-      const results = await tasks.get(testTask.id);
-
-      expect(results).toBeTruthy();
-      expect(results.id).toBe(testTask.id);
-    });
-  });
-
-  describe("delete", () => {
-    test("should delete a task", async () => {
-      const results = await tasks.delete(testTask.id);
-
-      expect(results).toBeNull();
-    });
+  test("should run tasks in sequence", async () => {
+    // Test that tasks is created
+    expect(tasks).toBeTruthy();
+  
+    // Test task creation
+    const createResults = await tasks.create(ReclaimTaskCreateMock);
+    expect(createResults).toBeTruthy();
+    expect(createResults.id).toBeGreaterThan(0);
+  
+    // Test task search
+    const searchResults = await tasks.search();
+    expect(searchResults).toBeTruthy();
+    expect(searchResults.length).toBeGreaterThan(0);
+  
+    // Test task search by title
+    const filterResults = await tasks.search({ title: ReclaimTaskCreateMock.title });
+    expect(filterResults).toBeTruthy();
+    expect(filterResults.length).toBeGreaterThan(0);
+    expect(filterResults[0].id).toBeGreaterThan(0);
+    expect(filterResults[0].title).toBe(ReclaimTaskCreateMock.title);
+  
+    // Test task update
+    const updateResults = await tasks.update(createResults.id, { title: "Updated Test Task 1" });
+    expect(updateResults).toBeTruthy();
+    expect(updateResults.id).toBe(updateResults.id);
+    expect(updateResults.title).toBe("Updated Test Task 1");
+  
+    // Test task get
+    const getResults = await tasks.get(createResults.id);
+    expect(getResults).toBeTruthy();
+    expect(getResults.id).toBe(createResults.id);
+  
+    // Test task delete
+    const deleteResults = await tasks.delete(createResults.id);
+    expect(deleteResults).toBeNull();
   });
 });
